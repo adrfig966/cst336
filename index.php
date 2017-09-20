bd8543328ffb5062bfc2c45a88d10e6b093fccfd
Adrian Figueroa <adrfigueroa@csumb.edu>
2017-09-20 07:32:00 +0000

    First commit of my first 3 assignments because I didn't follow directions

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       
+    </head>
+    <body>
+            <a href="lab2-pcp/index.php">Lab 2</a>
+            <a href="lab1-personalportfolio/index.html">Lab 1</a>
+            <a href="hw1/index.html">Homework 1</a>
+    </body>
+</html>
---
+++ lab2-pcp/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+            include 'inc/functions.php';
+        ?>
+        <link rel="stylesheet" href="css/styles.css"/ type="text/css">
+        <title> 777 Slot Machine </title>
+    </head>
+    <body>
+        <div id="main">        
+            <?php
+                play();
+            ?>
+            <form>
+                <input type="submit" value="Spin!"/>
+            </form>
+        </div>
+
+    </body>
+</html>

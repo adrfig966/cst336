6b041ac4662ad3b08785a2f45cb2e937587a4d65
Adrian Figueroa <adrfigueroa@csumb.edu>
2017-09-25 21:55:57 +0000

    Forgot to update index.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
             <a href="lab2-pcp/index.php">Lab 2</a>
             <a href="lab1-personalportfolio/index.html">Lab 1</a>
             <a href="hw1/index.html">Homework 1</a>
+            <a href="hw2/index.php">Homework 1</a>
     </body>
 </html>

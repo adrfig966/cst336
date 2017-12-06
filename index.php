f0b57837d2bab9fbccee3337cab8e6b652de64d4
Adrian Figueroa <adrfigueroa@csumb.edu>
2017-12-06 21:06:13 +0000

    All my work except lab 8 and 9

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
     <head>
     </head>
     <body>
+            
+     
             <a href="lab7/index.php">Lab 7</a> Make sure the url for this one is http and not https or the API call wont work :(
             <br>
             <a href="lab6/index.php">Lab 6</a>
@@ -11,6 +13,9 @@
             <a href="lab3/index.php">Lab 3</a>
             <a href="lab2/index.php">Lab 2</a>
             <a href="lab1-personalportfolio/index.html">Lab 1</a>
+            <br>
+            <a href="hw5/index.php">Homework 5</a> I think for this one too, it should be http 
+            <br>
             <a href="hw4/index.php">Homework 4</a>
             <a href="hw3/index.php">Homework 3</a>
             <a href="hw2/index.php">Homework 2</a>
